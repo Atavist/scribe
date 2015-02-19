96570df9898bbf21a0f563fc963c5673921fcffe
Jeff Sisson <jeffsisson@gmail.com>
2015-02-18 20:11:07 -0500

    producing custom build for umd-style use case

---
+++ umd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# PREPARES SCRIBE + SELECTED SUBSET OF PLUGINS FOR UMD
+
+echo "" > build.js
+
+browserify --global-transform deamdify --standalone Scribe . >> build.js
+browserify --global-transform deamdify --standalone scribePluginBlockquoteCommand "bower_components/scribe-plugin-blockquote-command" >> build.js
+browserify --global-transform deamdify --standalone scribePluginUnlink "bower_components/scribe-plugin-intelligent-unlink-command" >> build.js
+browserify --global-transform deamdify --standalone scribePluginUnlink "bower_components/scribe-plugin-intelligent-unlink-command" >> build.js
+browserify --global-transform deamdify --standalone scribePluginFormatterPlainTextConvertNewLinesToHtml "bower_components/scribe-plugin-formatter-plain-text-convert-new-lines-to-html" >> build.js
+browserify --global-transform deamdify --standalone scribePluginSanitizer "bower_components/scribe-plugin-sanitizer" >> build.js
+browserify --global-transform deamdify --standalone scribePluginHeadingCommand "bower_components/scribe-plugin-heading-command" >> build.js
